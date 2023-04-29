40d6841a31db95913b3b878057250de82bf270cb
Marshall <marshallfolkman@gmail.com>
2023-04-29 15:48:03 -0600

    more link fighting

--- W5.py
+++ W5.py
@@ -1,3 +1,5 @@
+#https://raw.githubusercontent.com/Vamboozer/AI/main/W5.py
+
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
@@ -57,7 +59,7 @@ def view_digit(ex, label=None, feature=None):
     plt.xticks([]), plt.yticks([])
     
 #data = ThreesandEights("data/mnist21x21_3789.pklz")
-url = "https://github.com/Vamboozer/AI/blob/cf482ca9e0f0a8c5829f03b4d518e360585fbae8/mnist21x21_3789.pkl"
+url = "https://raw.githubusercontent.com/Vamboozer/AI/main/mnist21x21_3789.pkl"
 data = ThreesandEights(url)
 
 view_digit(data.x_train[0], data.y_train[0])
